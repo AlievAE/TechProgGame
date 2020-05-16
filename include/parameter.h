4b8b1e3d371f0c82211e511d3b84933127ae7094
AlievAE <AlievAE@phystech.edu>
2020-05-17 02:28:15 +0600

    cosmetic changes

--- include/parameter.h
+++ include/parameter.h
@@ -8,7 +8,6 @@ public:
     const int SHIELD[3] = {1, 2, 3};
     const int LANCER_VISION[3] = {1, 2, 3};
     const int LANCER_BOUNTY[3] = {3, 2, 1};
-    const int LANCER_COST[3] = {2, 4, 6};
     const int LANCER_ATTACK_RANGE[3] = {2, 2, 3};
     const int LANCER_DAMAGE[3] = {3, 6, 9};
     const int LANCER_MARCH_SPEED[3] = {2, 2, 2};
@@ -20,7 +19,6 @@ public:
     const int ARCHER_SPEED[3] = {1, 2, 3};
     const int ARCHER_VISION[3] = {4, 6, 8};
     const int ARCHER_BOUNTY[3] = {5, 3, 1};
-    const int ARCHER_COST[3] = {3, 6, 9};
     const int ARCHER_ATTACK_RANGE[3] = {5, 7, 9};
     const int ARCHER_DAMAGE[3] = {2, 6, 10};
     const int ARCHER_MIN_RANGE[3] = {5, 4, 3};
@@ -30,7 +28,6 @@ public:
     const int CAVALRY_LANCER_SPEED[3] = {3, 3, 3};
     const int CAVALRY_LANCER_VISION[3] = {1, 2, 3};
     const int CAVALRY_LANCER_BOUNTY[3] = {10, 8, 6};
-    const int CAVALRY_LANCER_COST[3] = {10, 20, 30};
     const int CAVALRY_LANCER_DAMAGE[3] = {7, 10, 13};
     const int CAVALRY_LANCER_ATTACK_RANGE[3] = {2, 2, 3};
     const int CAVALRY_LANCER_BERSERK[5] = {2, 4, 6, 9, 13};
@@ -39,7 +36,6 @@ public:
     const int CAVALRY_ARCHER_SPEED[3] = {1, 2, 3};
     const int CAVALRY_ARCHER_VISION[3] = {4, 6, 8};
     const int CAVALRY_ARCHER_BOUNTY[3] = {5, 3, 1};
-    const int CAVALRY_ARCHER_COST[3] = {3, 6, 9};
     const int CAVALRY_ARCHER_ATTACK_RANGE[3] = {5, 7, 9};
     const int CAVALRY_ARCHER_DAMAGE[3] = {2, 6, 10};
     const int CAVALRY_ARCHER_MIN_RANGE[3] = {3, 2, 1};
@@ -50,6 +46,11 @@ public:
 
     const int BUFF_COST[4] = {5, 6, 7, 8};
 
+
+    //const int LANCER_COST[3] = {2, 4, 6};
+    //const int ARCHER_COST[3] = {3, 6, 9};
+    //const int CAVALRY_LANCER_COST[3] = {10, 20, 30};
+    //const int CAVALRY_ARCHER_COST[3] = {3, 6, 9};
     int COSTS[4][3] = {{2, 4, 6}, {3, 6, 9}, {10, 20, 30}, {3, 6, 9}};
 
     friend class Singleton;
